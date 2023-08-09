7944070a55a9d290a06e2f9ea2092ba8b5503234
Ritam Chatterjee <85054086+regnna@users.noreply.github.com>
2023-08-10 00:35:41 +0530

    ggs

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def main():
   # else:
   #       print("OPENAI_API_KEY is set")
   st.markdown("<h1 style='text-align: center; color: white;'>Ask your csv</h1>",unsafe_allow_html=True)
-  st.markdown(" <a href='https://platform.openai.com/account/api-keys'>openai API key</a>",unsafe_allow_html=True)
+  st.markdown("<span style='color:black' <a href='https://platform.openai.com/account/api-keys' target='_blank'>openai API key</a>></span>",unsafe_allow_html=True)
   openaikey=st.text_input("Enter your <a href='https://platform.openai.com/account/api-keys'>openai API key</a>",unsafe_allow_html=True)
 
   # st.header("Ask Your CSV ")
